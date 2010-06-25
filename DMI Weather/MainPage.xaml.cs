8850fa7eca9a48df3c1271b8e01c25d48cff9a37
Claus Jørgensen <thedeathart@gmail.com>
2010-06-25 05:41:00 +0200

    Initial Commit

---
+++ DMI Weather/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace DMI_Weather
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            SupportedOrientations = SupportedPageOrientation.Portrait | SupportedPageOrientation.Landscape;
+        }
+
+        private void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is Image)
+            {
+                var image = sender as Image;
+
+                if ((image.ActualWidth > 0) && (image.ActualHeight > 0))
+                {
+                    image.Clip = new RectangleGeometry()
+                    {
+                        Rect = new Rect(2, 2, image.ActualWidth - 4, image.ActualHeight - 4)
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ DMI Weather/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vejrudsigten fra DMI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © Claus Jørgensen 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71f6d153-0759-4d6e-9bcd-de9a49d8f232")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
